260045c3114246225613e35f0423489688b8ef58
Srikahn <49712202+Srikahn@users.noreply.github.com>
2022-08-06 17:34:30 +0200

    Create conf.model.php

---
+++ inc/conf.model.php
@@ -0,0 +1,17 @@
+<?php
+
+// Modify the following constants according to your own web server configuration
+
+define("DB_NAME", "");
+
+define("DB_USER", "");
+
+define("DB_PASSWORD", "");
+
+define("ROOT", $_SERVER["DOCUMENT_ROOT"]);
+
+define("ROOTHTML", "");
+
+define("SERVER", "https://" . $_SERVER['HTTP_HOST']);
+
+$pdo = new PDO('mysql:dbname=' . DB_NAME . ';host=localhost', DB_USER, DB_PASSWORD);
